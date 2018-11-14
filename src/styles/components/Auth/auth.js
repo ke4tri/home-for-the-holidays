7928641f71bad79b6cb382df9390f2407e05c753
Wayne Collier <ke4tri@gmail.com>
2018-11-13 20:04:00 -0600

    google button

--- src/styles/components/Auth/auth.js
+++ src/styles/components/Auth/auth.js
@@ -1,12 +1,14 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import googleImage from './googleLogin.png';
 import './auth.scss';
 
 
 const loginButton = () => {
   const domString = `
-  <button id="google-auth" class="btn btn-secondary">login</button>`;
+  <button id="google-auth" class="btn btn-secondary">
+  <img src="${googleImage}"/></button>`;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
     const provider = new firebase.auth.GoogleAuthProvider();
