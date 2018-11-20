05bc6eadfad9d6008cb3c4f7ff43caecdd8a608f
Wayne Collier <ke4tri@gmail.com>
2018-11-19 20:59:40 -0600

    delete

--- src/styles/components/FriendsPage/friendsPage.js
+++ src/styles/components/FriendsPage/friendsPage.js
@@ -3,15 +3,50 @@ import $ from 'jquery';
 import apiKeys from '../../../../db/apiKeys.json';
 import authHelpers from '../../../helpers/authHelpers';
 
+
+const printSingleFriend = (friend) => {
+  const friendString = `
+  <div>
+    <h1>${friend.name}</h1>
+    <h3>${friend.relationship}</h3>
+    <p>${friend.address}</p>
+    <p>${friend.email}</p>
+    <p>${friend.phoneNumber}</p>
+    <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
+    </div>
+    `;
+  $('#single-container').html(friendString);
+};
+
+const getSingleFriend = (e) => {
+  console.log('EEEEE');
+  // firebase id
+  const friendId = e.target.dataset.dropdownId;
+  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends/${friendId}.json`)
+    .then((result) => {
+      const singleFriend = result.data;
+      singleFriend.id = friendId;
+      printSingleFriend(singleFriend);
+    })
+    .catch((error) => {
+      console.error('error in getting one friend', error);
+    });
+};
+
 const buildDropdown = (friendsArray) => {
   let dropdown = `<div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Pick a Friend
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
-  friendsArray.forEach((friend) => {
-    dropdown += `<div class="dropdown-item">${friend.name}</div>`;
-  });
+  if (friendsArray.length) {
+    friendsArray.forEach((friend) => {
+      dropdown += `<div class="dropdown-item get-single"data-dropdown-id="${friend.id}">${friend.name}</div>`;
+    });
+  } else {
+    dropdown += '<div class="dropdown-item">You have no friends.</div>';
+  }
+
   dropdown += '</div></div>';
   $('#dropdown-container').html(dropdown);
 };
@@ -35,4 +70,27 @@ const friendsPage = () => {
     });
 };
 
-export default friendsPage;
+const deleteFriend = (e) => {
+  // firebase id
+  const idToDelete = e.target.dataset.deleteId;
+  axios.delete(`${apiKeys.firebaseKeys.databaseURL}/friends/${idToDelete}.json`)
+    .then(() => {
+      friendsPage();
+      $('#single-container').html('');
+    })
+    .catch((error) => {
+      console.error('error in deleting friedn', error);
+    });
+};
+
+const bindEvents = () => {
+  $('body').on('click', '.get-single', getSingleFriend);
+  $('body').on('click', '.delete-btn', deleteFriend);
+};
+
+const initializeFriendsPage = () => {
+  friendsPage();
+  bindEvents();
+};
+
+export default initializeFriendsPage;
