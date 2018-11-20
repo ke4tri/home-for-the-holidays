7352d413526109a59808b37461e7c3171f7667db
Wayne Collier <ke4tri@gmail.com>
2018-11-19 21:26:20 -0600

    adding friend

---
+++ src/styles/components/AddEditFriends/addEditFriends.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import authHelpers from '../../../helpers/authHelpers';
+
+const formBuilder = () => {
+  const form = `
+  <div class="form-group">
+    <label for="form-friend-name">Name:</label>
+    <input type="text" class="form-control" id="form-friend-name"  placeholder="John Smith">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-address">Address:</label>
+    <input type="text" class="form-control" id="form-friend-address" placeholder="Hell">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-email">Email:</label>
+    <input type="text" class="form-control" id="form-friend-email" placeholder="fake@gmail.com">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-phone">Phone Number:</label>
+    <input type="text" class="form-control" id="form-friend-phone" placeholder="123-123-1234">
+  </div>
+  <div class="form-group">
+    <label for="form-friend-relationship">Relationship:</label>
+    <input type="text" class="form-control" id="form-friend-relationship" placeholder="I don't know them">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+`;
+  return form;
+};
+
+const gettingFriendFromForm = () => {
+  const friend = {
+    name: $('#form-friend-name').val(),
+    address: $('#form-friend-address').val(),
+    email: $('#form-friend-email').val(),
+    relationship: $('#form-friend-relationship').val(),
+    phoneNumber: $('#form-friend-name').val(),
+    isAvoiding:false,
+    uid: 
+  }
+}
