1ae498b9d4cc00cda085ff19daf1d160032082ca
Wayne Collier <ke4tri@gmail.com>
2018-11-20 20:44:40 -0600

    edit button working for updating friends

--- src/styles/components/AddEditFriends/addEditFriends.js
+++ src/styles/components/AddEditFriends/addEditFriends.js
@@ -3,23 +3,23 @@ import authHelpers from '../../../helpers/authHelpers';
 import friendsData from '../../../helpers/data/friendsData';
 import initializeFriendsPage from '../FriendsPage/friendsPage';
 
-const formBuilder = () => {
+const formBuilder = (friend) => {
   const form = `
   <div class="form-group">
     <label for="form-friend-name">Name:</label>
-    <input type="text" class="form-control" id="form-friend-name"  placeholder="John Smith">
+    <input type="text" class="form-control" value="${friend.name}" id="form-friend-name"  placeholder="John Smith">
   </div>
   <div class="form-group">
     <label for="form-friend-address">Address:</label>
-    <input type="text" class="form-control" id="form-friend-address" placeholder="Hell">
+    <input type="text" class="form-control" value="${friend.address}" id="form-friend-address" placeholder="Hell">
   </div>
   <div class="form-group">
     <label for="form-friend-email">Email:</label>
-    <input type="text" class="form-control" id="form-friend-email" placeholder="fake@gmail.com">
+    <input type="text" class="form-control" value="${friend.email}" id="form-friend-email" placeholder="fake@gmail.com">
   </div>
   <div class="form-group">
     <label for="form-friend-phone">Phone Number:</label>
-    <input type="text" class="form-control" id="form-friend-phone" placeholder="123-123-1234">
+    <input type="text" class="form-control" value="${friend.phoneNumber}" id="form-friend-phone" placeholder="123-123-1234">
   </div>
   <div class="form-group">
     <label for="form-friend-relationship">Relationship:</label>
@@ -44,8 +44,15 @@ const gettingFriendFromForm = () => {
 };
 
 const buildAddForm = () => {
+  const emptyFriend = {
+    name: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    relationship: '',
+  };
   let domString = '<h2>Add New Friend</h2>';
-  domString += formBuilder();
+  domString += formBuilder(emptyFriend);
   domString += '<button id="add-friend">Save New Friend</button>';
   $('#add-edit-friend').html(domString).show();
   $('#friends').hide();
@@ -64,6 +71,23 @@ const addNewFriend = () => {
     });
 };
 
+// Edit
+const showEditForm = (e) => {
+  const idToEdit = e.target.dataset.editId;
+  friendsData.getSingleFriend(idToEdit)
+    .then((singleFriend) => {
+      let domString = '<h2>Edit Friend</h2>';
+      domString += formBuilder(singleFriend);
+      domString += '<button id="edit-friend">Save Friend</button>';
+      $('#add-edit-friend').html(domString).show();
+      $('#friends').hide();
+    })
+    .catch((error) => {
+      console.error('error in getting single for edit', error);
+    });
+};
+
 $('body').on('click', '#add-friend', addNewFriend);
+$('body').on('click', '.edit-btn', showEditForm);
 
 export default buildAddForm;
